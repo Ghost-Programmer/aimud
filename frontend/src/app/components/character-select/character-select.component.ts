a9cbbc0de815271512f591fb5af877703602be9d
Jeff Miller <jeff@mymiller.name>
2026-03-08 16:07:15 -0400

    feat: enhance character selection UI with resource bars and improved layout

--- frontend/src/app/components/character-select/character-select.component.ts
+++ frontend/src/app/components/character-select/character-select.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharacterService } from '../../services/character.service';
+import { ConfigService } from '../../services/config.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-character-select',
@@ -13,20 +15,31 @@ export class CharacterSelectComponent implements OnInit {
   @Input() excludeCharacterIds: number[] = [];
   @Output() characterSelected = new EventEmitter<any>();
   characters: any[] = [];
+  races: any[] = [];
+  classes: any[] = [];
 
-  constructor(private characterService: CharacterService) {}
+  constructor(
+    private characterService: CharacterService,
+    private configService: ConfigService
+  ) {}
 
   ngOnInit() {
-    this.loadCharacters();
+    this.loadData();
   }
 
-  loadCharacters() {
-    this.characterService.getCharacters().subscribe({
-      next: (data) => {
-        this.characters = data;
+  loadData() {
+    forkJoin({
+      characters: this.characterService.getCharacters(),
+      races: this.configService.getAllRaces(),
+      classes: this.configService.getAllCharacterClasses()
+    }).subscribe({
+      next: ({ characters, races, classes }) => {
+        this.characters = characters;
+        this.races = races;
+        this.classes = classes;
       },
       error: (error) => {
-        console.error('Error fetching characters', error);
+        console.error('Error loading data', error);
       }
     });
   }
@@ -37,5 +50,15 @@ export class CharacterSelectComponent implements OnInit {
 
   selectCharacter(character: any) {
     this.characterSelected.emit(character);
+  }
+
+  getRaceName(raceId: number): string {
+    const race = this.races.find(r => r.id === raceId);
+    return race ? race.name : 'Unknown';
+  }
+
+  getClassName(classId: number): string {
+    const cls = this.classes.find(c => c.id === classId);
+    return cls ? cls.name : 'Unknown';
   }
 }
