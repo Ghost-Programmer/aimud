ff5976623cccbf0d402f5a9bbec727fdb0364771
Jeff Miller <jeff@mymiller.name>
2026-03-12 16:52:17 -0400

    feat: enhance logout command to deselect character on execution

--- src/main/java/com/aimud/aimud/commands/Logout.java
+++ src/main/java/com/aimud/aimud/commands/Logout.java
@@ -2,12 +2,21 @@ package com.aimud.aimud.commands;
 
 import com.aimud.aimud.annontation.MudCommand;
 import com.aimud.aimud.model.Character;
+import com.aimud.aimud.service.CharacterService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 
+@RequiredArgsConstructor
 @MudCommand(name = "logout")
 public class Logout implements Command{
+
+    private final CharacterService characterService;
+
     @Override
     public void execute(Character character, String commandLine) {
+        characterService.deselectCharacter(character.getId());
 
+        //TODO: Tell user logged out
     }
 }
